ea550c707ae5bb1dbc795d811ffe219b9c004df0
Pasindiweerathunge <dmppw96@gmail.com>
2022-04-12 14:38:40 +0530

    Update Book ( controller, route)

--- routes/book-routes.js
+++ routes/book-routes.js
@@ -1,20 +1,10 @@
 const express = require("express");
 const router = express.Router();
 const Book = require("../models/book");
+const bookcontroller  = require ("../controllers/book-controller");
 
 
-router.get("/",async(req,res,next)=>{
-    let books;
-    try{
-        books = await Book.find();
-    }catch(err){
-        console.log(err);
-    }
-
-    if(!books){
-        return res.status(404).json({message: "No Products fund"});
-    }
-    return res.status(200).json({books});
-});
+router.get("/",bookcontroller.getAllBooks);
+    
 
 module.exports = router;
