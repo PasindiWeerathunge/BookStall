c740031c78ae1238b45f92d16f8f864d3bf78cda
Pasindiweerathunge <dmppw96@gmail.com>
2022-04-12 14:17:31 +0530

    Create book (book - route, model)

--- app.js
+++ app.js
@@ -1,11 +1,9 @@
 const express = require('express');
 const mongoose = require('mongoose');
-
+const router = require("./routes/book-routes");
 const app = express();
 
-app.use('/',(req,res,next)=> {
-    res.send("this is starting application");
-});
+app.use("/books",router);
 
 mongoose.connect(
     "mongodb+srv://DMppw96:ka003pa005@bookstall.y78g0.mongodb.net/bookStalldb?retryWrites=true&w=majority"
---
+++ routes/book-routes.js
@@ -0,0 +1,20 @@
+const express = require("express");
+const router = express.Router();
+const Book = require("../models/book");
+
+
+router.get("/",async(req,res,next)=>{
+    let books;
+    try{
+        books = await Book.find();
+    }catch(err){
+        console.log(err);
+    }
+
+    if(!books){
+        return res.status(404).json({message: "No Products fund"});
+    }
+    return res.status(200).json({books});
+});
+
+module.exports = router;
